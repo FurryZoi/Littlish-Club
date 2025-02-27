cc236cecbf66017acf336b68d12a2e434c347c9b
Furry Zoi <volkof06@mail.ru>
2025-02-28 01:58:21 +0300

    Fix: mod button position, bookmark link

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const MOD_VERSION: string = "1.0.0";
 export const MOD_NAME: string = "Littlish Club";
 export const MOD_FULL_NAME: string = MOD_NAME;
 export const REPO_URL: string = "https://github.com/FurryZoi/Littlish-Club";
-export const MOD_BUTTON_POSITION: RectTuple = [1705, 75, 90, 90];
+export const MOD_BUTTON_POSITION: RectTuple = [1705, 765, 90, 90];
 export const CANVAS_BABIES_APPEARANCES: {
     name: string
     creator: string
